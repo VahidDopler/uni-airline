4655d22684cd159ec91c71063efba0877d17c602
Vahid Dopler <77194408+VahidDopler@users.noreply.github.com>
2023-11-30 20:09:07 +0330

    first commit

--- flightmanager/urls.py
+++ flightmanager/urls.py
@@ -1,9 +1,8 @@
 from django.urls import path
-from . import views
-from django.urls import path
 
 from . import views
 
 urlpatterns = [
     path("all/", views.FlightListView.as_view(), name="all_flights"),
+    path("search", views.search_flights, name="search_flight")
 ]
--- flightmanager/views.py
+++ flightmanager/views.py
@@ -1,6 +1,7 @@
+from django.shortcuts import render
 from django.views.generic import ListView
 
-from flightmanager.models import Flight
+from flightmanager.models import Flight, Airport
 
 
 # flight list
@@ -13,6 +14,23 @@ class FlightListView(ListView):
         context['flights'] = Flight.objects.all()
         return context
 
+
+# search flight page
+def search_flights(request):
+    if request.method == 'GET':
+        all_air = Airport.objects.all()
+        errors = {'origin': 'Please select an origin airport.',
+                  'destination': 'Please select a destination airport.'}
+        context = {'errors': errors, 'origin_airports': all_air, 'destination_airports': all_air}
+        return render(request, 'flights/search.html', context)
+    if request.method == "POST":
+        print(request.POST.get("origin"))
+        print(request.POST.get("destination"))
+        all_air = Airport.objects.all()
+        errors = {'origin': 'Please select an origin airport.',
+                  'destination': 'Please select a destination airport.'}
+        context = {'errors': errors, 'origin_airports': all_air, 'destination_airports': all_air}
+        return render(request, 'flights/search.html', context)
 # each flight page
 
 
